ca61a769e9c817350e3b3dee51f75cedbb990a5b
NANO <File changed>
2022-07-03 13:35:18 +0600

    The eighth attempt

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,16 +3,7 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-
         int value = 1000;
-
         int value2 = 3000;
-        int variables = 5000;
-
-
-
-        int myInteger = 1000000;
-        int myVar = 1234567;
-
     }
 }
