1a104cf7661f729b1417f0b9058a3f4d69dab862
NANO <File changed>
2022-07-03 11:57:56 +0600

    The third attempt

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -13,6 +13,6 @@ public class CopiedNames {
         int intFour= 100000;
         int myInteger = 1000000;
         int myVar = 1234567;
-        int myIntegerOne = 1234567;
+        int myIntegerTwo = 1234567;
     }
 }
