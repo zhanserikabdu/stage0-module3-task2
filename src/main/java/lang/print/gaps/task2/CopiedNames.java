2b2449b135fb5a603af159222e26a5f9e1cf6252
NANO <File changed>
2022-07-03 12:33:56 +0600

    The fourth attempt

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,16 +3,16 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variableOne = 2000;
+        variable = 2000;
         int value = 1000;
-        int variableTwo = 4000;
+        variable = 4000;
         int value2 = 3000;
         int variables = 5000;
-        int variablesOne = 7000;
-        int valueThree = 90000;
-        int intFour= 100000;
+        variables = 7000;
+        value = 90000;
+
         int myInteger = 1000000;
         int myVar = 1234567;
-        int myIntegerTwo = 1234567;
+        myInteger = 1234567;
     }
 }
--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
 
     void callToFriend() {
-
+        //some code
     }
 
     void callByNumber(int number) {
-
+        //some code
     }
 }
