8f278be44cda1876cc7de719ddabdbccaa78dd03
NANO <File changed>
2022-07-03 13:53:00 +0600

    The last attempt

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int morrow = 0;
-        int $money = 10;
+        int money = 10;
         int hundred = 100;
         int back = 1000;
-        int _underscore = 10000;
+        int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-
+        int badExampleOfVarName = 1000000;
     }
 }
